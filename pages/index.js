15b4ba0ccfc5fe908111f962469307d8922e1876
ThePlugJumbo <chnxcx@gmail.com>
2022-12-25 14:00:52 -0800

    fixed landing mobile bugs

--- pages/index.js
+++ pages/index.js
@@ -57,14 +57,14 @@ export default function Home() {
               <h1 className="lg:text-6xl md:text-5xl text-4xl font-semibold text-gray-900 mb-8">
                 Turn your profile picture into a comic book style portrait
               </h1>
-              <p className="md:text-lg text-gray-500 mb-14">
+              <p className="md:text-lg text-gray-500 mb-10">
                 Cartoonify yourself for free!  Create a free cartoon profile picture, then easily export the photo to use or share on social media.
               </p>
-              <div className="my-4">
-                <Link href="/tool" className="px-8 py-4 mr-4 bg-violet-500 hover:bg-violet-400 text-white rounded transition">
-                  Cartoonify Yourself
+              <div className="flex md:flex-row flex-col md:justify-items-start justify-items-center md:items-start items-center gap-4">
+                <Link href="/tool" className="inline-block px-8 py-4 bg-violet-500 hover:bg-violet-400 text-white rounded transition whitespace-nowrap">
+                  Launch Tool
                 </Link>
-                <Link href="/" className="px-8 py-4 text-gray-900 underline-offset-2 decoration-2 rounded hover:underline">
+                <Link href="/" className="inline-block px-8 py-4 text-gray-900 underline-offset-2 decoration-2 rounded hover:underline whitespace-nowrap">
                   How It Works
                 </Link>
               </div>
@@ -77,7 +77,7 @@ export default function Home() {
         <section className="px-3">
           <div className="max-w-screen-xl mx-auto md:py-20 pt-6 pb-14">
             <h2 className="md:text-4xl text-3xl font-semibold text-gray-900 md:mb-20 mb-14">
-              <span className="underline underline-offset-[1rem] decoration-4 decoration-violet-500">Cartoonify</span> yourself for free
+              <span className="overline decoration-4 decoration-violet-500">Cartoonify</span> yourself for free
             </h2>
             <ul className="grid md:grid-cols-3 gap-12">
               <li className="space-y-4">
@@ -117,7 +117,7 @@ export default function Home() {
           </div>
         </section>
         <section className="px-3 border-b border-gray-300">
-          <div className="grid md:grid-cols-2 items-center md:gap-20 gap-2 max-w-screen-xl mx-auto md:pt-20 md:pb-28 pt-6 pb-[5.5rem]">
+          <div className="grid md:grid-cols-2 items-center md:gap-20 gap-2 max-w-screen-xl mx-auto md:pt-20 md:pb-28 pt-6 pb-14">
             <div className="md:p-0 p-10 relative md:order-1 order-2">
               <div className="absolute md:left-8 left-[4.5rem] md:top-8 top-[4.5rem] w-full md:max-w-[500px] md:w-full max-w-[500px] w-[calc(100%-5rem)] md:h-[100%] h-[calc(100%-5rem)] bg-violet-500 -z-10" />
               <div className="max-w-[500px]">
@@ -128,14 +128,14 @@ export default function Home() {
               <h3 className="md:text-4xl text-3xl font-semibold text-gray-900 mb-8">
                 The perfect portrait for profile pictures, resumes, and more
               </h3>
-              <p className="text-gray-500 mb-14">
+              <p className="text-gray-500 mb-10">
                 Generate a professional, while fun image to use on any other platform.  Simply upload your best photo, configure the tool how you want, and click generate.
               </p>
-              <div className="my-4">
-                <Link href="/tool" className="px-8 py-4 mr-4 bg-violet-500 hover:bg-violet-400 text-white rounded transition">
+              <div className="flex md:flex-row flex-col md:justify-items-start justify-items-center md:items-start items-center gap-4">
+                <Link href="/tool" className="inline-block px-8 py-4 bg-violet-500 hover:bg-violet-400 text-white rounded transition whitespace-nowrap">
                   Launch Tool
                 </Link>
-                <Link href="/" className="px-8 py-4 text-gray-900 underline-offset-2 decoration-2 rounded hover:underline">
+                <Link href="/" className="inline-block px-8 py-4 text-gray-900 underline-offset-2 decoration-2 rounded hover:underline whitespace-nowrap">
                   Read The Blog
                 </Link>
               </div>
