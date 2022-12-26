f0d14a77da1345e83caff26bd9a16d13a507eaff
Tarun Samanta <tarunsamanta77@gmail.com>
2022-12-26 13:22:11 +0530

    changed as per your requirement

--- pages/tool.js
+++ pages/tool.js
@@ -34,7 +34,7 @@ export default function Tool() {
                         <div className="flex flex-col xl:w-[calc(100%-24rem)] lg:w-[calc(100%-20rem)] md:w-[calc(100%-18rem)] w-full" >
                             <label htmlFor="take-picture" className="flex justify-center items-center px-3 md:h-80 h-52 border-b border-gray-300 outline-gray-900 -outline-offset-2 hover:bg-gray-100 hover:outline-dotted cursor-pointer transition">
                                 <div className="text-center">
-                                    <h1 className="md:text-2xl font-semibold text-gray-900 mb-4">
+                                    <h1 className="text-3xl md:text-2xl font-semibold text-gray-900 mb-4">
                                         <svg className="inline-block w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
@@ -49,7 +49,7 @@ export default function Tool() {
                             </label>
                             <label htmlFor="upload-picture" className="flex justify-center items-center md:h-full h-72 px-3 outline-gray-900 -outline-offset-2 hover:bg-gray-100 hover:outline-dotted cursor-pointer transition">
                                 <div className="text-center">
-                                    <h2 className="md:text-2xl font-semibold text-gray-900 mb-4">
+                                    <h2 className="text-3xl md:text-2xl font-semibold text-gray-900 mb-4">
                                         Or upload an image
                                     </h2>
                                     <p className="text-gray-500">
