cedfa9375ed7616012bac27c7b731db2243c9160
ThePlugJumbo <chnxcx@gmail.com>
2022-12-26 17:07:18 -0800

    added Google Analytics and Microsoft Clarity

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,39 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const env = process.env
+
   return (
     <Html>
       <Head>
         <meta name="description" content="Turn your profile picture into a comic book style portrait! Cartoonify yourself for free." />
         <link rel="icon" href="/favicon.ico" />
+
+        {/* Google Analytics */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${env.GOOGLE_ANALYTICS_GID}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag("js", new Date());
+              gtag("config", ${env.GOOGLE_ANALYTICS_GID || "none"}, { page_path: window.location.pathname });
+            `,
+          }}
+        />
+
+        {/* Microsoft Clarity */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", ${env.MICROSOFT_CLARITY_ID || "none"});
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
