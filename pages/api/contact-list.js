c9a30e5bcdbe31692cb3fe4614f35143cbc549a2
ThePlugJumbo <chnxcx@gmail.com>
2022-12-29 16:53:31 -0800

    addded error handle

--- pages/api/contact-list.js
+++ pages/api/contact-list.js
@@ -1,18 +1,23 @@
 export default handler = async (req, res) => {
     const email = req.body.email
 
-    await fetch(`https://${process.env.MAILCHIMP_API_SERVER}.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_AUDIENCE_ID}/members`, {
-        body: JSON.stringify({
-            email_address: email,
-            status: 'subscribed',
-        }),
-        headers: {
-            Authorization: `apikey ${process.env.MAILCHIMP_API_KEY}`,
-            'Content-Type': 'application/json',
-        },
-        method: 'POST',
-        }
-    )
+    try{
+        await fetch(`https://${process.env.MAILCHIMP_API_SERVER}.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_AUDIENCE_ID}/members`, {
+            body: JSON.stringify({
+                email_address: email,
+                status: 'subscribed',
+            }),
+            headers: {
+                Authorization: `apikey ${process.env.MAILCHIMP_API_KEY}`,
+                'Content-Type': 'application/json',
+            },
+            method: 'POST'
+        })
 
-    res.status(200).json({ 'Status': 'Success', 'APIKEY': process.env.MAILCHIMP_API_KEY})
+        res.status(200).json({ 'Status': 'Success', 'APIKEY': process.env.MAILCHIMP_API_KEY})
+    } catch (err) {
+        res.status(200).json({ 'Error': err})
+    }
+    
+    
 }
