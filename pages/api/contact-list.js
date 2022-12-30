94702ac71c8cd65c8138f37d16cfd52716a45512
ThePlugJumbo <chnxcx@gmail.com>
2022-12-29 16:48:17 -0800

    added async

--- pages/api/contact-list.js
+++ pages/api/contact-list.js
@@ -1,7 +1,7 @@
-export default function handler(req, res) {
+export default handler = async (req, res) => {
     const email = req.body.email
 
-    fetch(`https://${process.env.MAILCHIMP_API_SERVER}.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_AUDIENCE_ID}/members`, {
+    await fetch(`https://${process.env.MAILCHIMP_API_SERVER}.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_AUDIENCE_ID}/members`, {
         body: JSON.stringify({
             email_address: email,
             status: 'subscribed',
